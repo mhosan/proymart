0327ac186038f98b0a5f096f96884ed240c39386
Sergio Marcelo Hosan <marcelo.hosan@arba.gov.ar>
2025-02-26 16:31:43 -0300

    agregado de politica de privacidad

--- src/app/components/privacidad/privacidad.component.ts
+++ src/app/components/privacidad/privacidad.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { environment } from '../../../environments/environment'
 
 @Component({
   selector: 'app-privacidad',
@@ -8,5 +9,5 @@ import { Component } from '@angular/core';
   styleUrl: './privacidad.component.css'
 })
 export class PrivacidadComponent {
-
+  appName = environment.appName;
 }
