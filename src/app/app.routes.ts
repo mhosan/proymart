aa3813f1c2f4ff43b106faf9dae8b3b6bcbb3081
Sergio Marcelo Hosan <marcelo.hosan@arba.gov.ar>
2025-02-26 15:52:30 -0300

    actu footer

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,12 +4,16 @@ import { FeaturesComponent } from './components/features/features.component';
 import { TestimonialsComponent } from './components/testimonials/testimonials.component';
 import { PricingComponent } from './components/pricing/pricing.component';
 import { CtaComponent } from './components/cta/cta.component';
+import { PrivacidadComponent } from './components/privacidad/privacidad.component';
+import { TerminosComponent } from './components/terminos/terminos.component';
 
 export const routes: Routes = [
-    { path: '', component: HeroComponent },
+  { path: '', component: HeroComponent },
   { path: 'features', component: FeaturesComponent },
   { path: 'testimonials', component: TestimonialsComponent },
   { path: 'pricing', component: PricingComponent },
   { path: 'contact', component: CtaComponent },
+  { path: 'privacidad', component: PrivacidadComponent },
+  { path: 'terminos', component: TerminosComponent},
   { path: '**', redirectTo: '' }
 ];
