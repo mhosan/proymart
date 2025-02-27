6f032ae4153be8916ba761b1556f241195cdba5a
Marcelo Hosan <mhosan@gmail.com>
2025-02-27 18:24:41 -0300

    agregado de un servicio de prueba. Se invoca desde el boton Comenzar ahora

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,14 +1,40 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterLink, RouterLinkActive } from '@angular/router';
-import { environment } from '../../../environments/environment'
+import { environment } from '../../../environments/environment';
+import { ApiService } from '../../services/api-service.service'; 
+import { HttpClientModule, HttpHeaders } from '@angular/common/http'; 
+
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [RouterLink, RouterLinkActive],
+  imports: [RouterLink, RouterLinkActive, HttpClientModule], 
   templateUrl: './header.component.html',
   styleUrl: './header.component.css'
 })
-export class HeaderComponent {
+export class HeaderComponent implements OnInit{
   appName = environment.appName;
+  data: any;
+
+  constructor(private apiService: ApiService) {} 
+
+  ngOnInit() {
+   
+  }
+
+  testApi() {
+    const headers = new HttpHeaders({
+      'Accept': 'application/json'
+    });
+
+    this.apiService.getData(headers).subscribe(
+      response => {
+        console.log(response);
+      },
+      error => {
+        console.error('Error al enviar los datos', error);
+      }
+    );
+  }
+
 }
---
+++ src/app/services/api-service.service.ts
@@ -0,0 +1,20 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ApiService {
+  private apiUrl = "https://httpbin.org/headers"; // -H "accept: application/json"; 
+
+  constructor(private http: HttpClient) {}
+
+  getData(headers?: HttpHeaders): Observable<any> {
+    return this.http.get<any>(this.apiUrl, { headers });
+  }
+
+  postData(data: any): Observable<any> {
+    return this.http.post<any>(`${this.apiUrl}/data`, data);
+  }
+}
