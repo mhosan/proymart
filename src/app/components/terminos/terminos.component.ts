e3780aa68956f5cc502dfe6ac11ab1cb6acd18fd
Marcelo Hosan <mhosan@gmail.com>
2025-02-27 17:57:37 -0300

    terminos del servicio

--- src/app/components/terminos/terminos.component.ts
+++ src/app/components/terminos/terminos.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { environment } from '../../../environments/environment'
 
 @Component({
   selector: 'app-terminos',
@@ -8,5 +9,5 @@ import { Component } from '@angular/core';
   styleUrl: './terminos.component.css'
 })
 export class TerminosComponent {
-
+  appName = environment.appName;
 }
