dd569611b1fd1d7cf4b66e4914a3885dedac3adf
Marcelo Hosan <mhosan@gmail.com>
2025-03-27 10:44:09 -0300

    cambio nombre a Dato Urbano

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,5 +1,5 @@
 export const environment = {
     production: false,
     environment: 'development',
-    appName: 'Pepe Advisors'
+    appName: 'Dato Urbano'
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
     production: true,
-    appName: 'Prop Advisors' 
+    appName: 'Dato Urbano' 
 };
