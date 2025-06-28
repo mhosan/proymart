a21f087c82b9c51c57d3d0fcd505b5e16fbfc731
Marcelo Hosan <mhosan@gmail.com>
2025-06-28 10:50:07 -0300

    ocultar footer para Frappe Gantt

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,18 +1,31 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Router, NavigationEnd } from '@angular/router';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './components/shared/header/header.component';
 import { FooterComponent } from './components/shared/footer/footer.component';
 
 @Component({
-    selector: 'app-root',
-    imports: [
-        RouterOutlet,
-        HeaderComponent,
-        FooterComponent
-    ],
-    templateUrl: './app.component.html',
-    styleUrl: './app.component.css'
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css'],
+  imports: [
+    RouterOutlet,
+    HeaderComponent,
+    FooterComponent,
+    CommonModule
+  ]
 })
 export class AppComponent {
   title = 'martilleros';
+  showFooter = true;
+
+  constructor(private router: Router) {
+    this.router.events.subscribe((event: any) => {
+      if (event instanceof NavigationEnd) {
+        // Oculta el footer solo en la ruta 'frappe'
+        this.showFooter = !event.urlAfterRedirects.startsWith('/frappe');
+      }
+    });
+  }
 }
